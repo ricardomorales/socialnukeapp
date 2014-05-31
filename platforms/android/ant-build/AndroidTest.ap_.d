0e561b7ee8829321511929206f803879edff589f
ricardomorales <ric.morales22@gmail.com>
2014-05-31 14:22:10 -0400

    Added Facebook plugin.

--- platforms/android/ant-build/AndroidTest.ap_.d
+++ platforms/android/ant-build/AndroidTest.ap_.d
@@ -1,14 +1,21 @@
 C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\AndroidTest.ap_ \
- : C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable\icon.png \
+ : C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable\close.png \
+C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable\facebook_icon.png \
+C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable\icon.png \
+C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-hdpi\close.png \
+C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-hdpi\facebook_icon.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-hdpi\icon.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-hdpi\ic_action_next_item.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-hdpi\ic_action_previous_item.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-hdpi\ic_action_remove.png \
+C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-ldpi\close.png \
+C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-ldpi\facebook_icon.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-ldpi\icon.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-mdpi\icon.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-mdpi\ic_action_next_item.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-mdpi\ic_action_previous_item.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-mdpi\ic_action_remove.png \
+C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-xhdpi\close.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-xhdpi\icon.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-xhdpi\ic_action_next_item.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-xhdpi\ic_action_previous_item.png \
@@ -18,16 +25,23 @@ C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-xxhdpi\ic_action_pr
 C:\xampp\htdocs\socialnukeapp\platforms\android\res\drawable-xxhdpi\ic_action_remove.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\res\values\strings.xml \
 C:\xampp\htdocs\socialnukeapp\platforms\android\res\xml\config.xml \
+C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable\close.png \
+C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable\facebook_icon.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable\icon.png \
+C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-hdpi\close.png \
+C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-hdpi\facebook_icon.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-hdpi\icon.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-hdpi\ic_action_next_item.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-hdpi\ic_action_previous_item.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-hdpi\ic_action_remove.png \
+C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-ldpi\close.png \
+C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-ldpi\facebook_icon.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-ldpi\icon.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-mdpi\icon.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-mdpi\ic_action_next_item.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-mdpi\ic_action_previous_item.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-mdpi\ic_action_remove.png \
+C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-xhdpi\close.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-xhdpi\icon.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-xhdpi\ic_action_next_item.png \
 C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\res\drawable-xhdpi\ic_action_previous_item.png \
