da5d75beb954f89873e30f5eeecc53a840fa93b0
ricardomorales <ric.morales22@gmail.com>
2014-08-10 15:34:37 -0400

    Readded files to .gitignore

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\AndroidManifest.xml : \
-C:\xampp\htdocs\socialnukeapp\platforms\android\AndroidManifest.xml \
--- platforms/android/ant-build/AndroidTest-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\AndroidTest-debug-unaligned.apk : \
-C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\AndroidTest.ap_ \
-C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\classes.dex \
-C:\xampp\htdocs\socialnukeapp\platforms\android\CordovaLib\ant-build\classes.jar \
