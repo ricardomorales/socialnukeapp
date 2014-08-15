47d88e1897ac121f1a0f34de45df595fa1c3cf67
ricardomorales <ric.morales22@gmail.com>
2014-08-14 21:58:06 -0400

    Revert "Readded files to .gitignore"
    This reverts commit da5d75beb954f89873e30f5eeecc53a840fa93b0.

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\AndroidManifest.xml : \
+C:\xampp\htdocs\socialnukeapp\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidTest-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\AndroidTest-debug-unaligned.apk : \
+C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\AndroidTest.ap_ \
+C:\xampp\htdocs\socialnukeapp\platforms\android\ant-build\classes.dex \
+C:\xampp\htdocs\socialnukeapp\platforms\android\CordovaLib\ant-build\classes.jar \
